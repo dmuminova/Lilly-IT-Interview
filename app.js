b0797ab030ca68f3e917709c09f9efd21737d9c9
dmuminova <125288957+dmuminova@users.noreply.github.com>
2024-02-27 21:42:51 +0000

    assessment

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
-const express = require('express')
-const path = require('path')
-const stocks = require('./stocks')
+const express = require('express');
+const path = require('path');
+const stocks = require('./stocks');
+const cors = require('cors'); 
 
-const app = express()
-app.use(express.static(path.join(__dirname, 'static')))
+const app = express();
 
-app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
-})
+// adding middleware to serve static files and enable CORS
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(cors());
 
-app.get('/stocks/:symbol', async (req, res) => {
-  const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
-})
+// endpoint to get the list of available stocks
+app.get('/stocks', async (req, res, next) => {
+  try {
+    const stockSymbols = await stocks.getStocks();
+    if (stockSymbols.length === 0) {
+      res.status(404).json({ error: 'Data not found! Please check the stocks data file.' });
+    } else {
+      res.json({ stockSymbols });
+    }
+  } catch (err) {
+    next(err); 
+  }
+});
 
-app.listen(3000, () => console.log('Server is running!'))
+// endpoint to get data for a specific stock
+app.get('/stocks/:symbol', async (req, res, next) => {
+  try {
+    const { params: { symbol } } = req;
+    const data = await stocks.getStockPoints(symbol, new Date());
+    res.json(data);
+  } catch (err) {
+    next(err); 
+  }
+});
+
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+// starting the server
+app.listen(3000, () => console.log('Server is running!'));
